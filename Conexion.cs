c23514de4a92e6d97c269ad8a46ed38767d3dabb
Daniel Jofré <danielfjofreb@gmail.com>
2024-05-03 13:00:16 -0400

    Agregar archivos de proyecto.

---
+++ Conexion.cs
@@ -0,0 +1,86 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportarSQLaExcel_UI
+{
+    public class Conexion
+    {
+        public bool Conecta(string connectionString, string query, string excelFilePath, string NombreHoja)
+        {
+            try
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                // Crear un objeto de conexión
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Crear un comando SQL
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Crear un adaptador de datos
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                    // Crear un DataSet para almacenar los resultados de la consulta
+                    DataSet dataSet = new DataSet();
+
+                    // Abrir la conexión y llenar el DataSet
+                    connection.Open();
+                    adapter.Fill(dataSet);
+
+                    // Crear un nuevo archivo de Excel
+                    using (var package = new OfficeOpenXml.ExcelPackage())
+                    {
+                        // Agregar una hoja al archivo de Excel
+                        var worksheet = package.Workbook.Worksheets.Add(NombreHoja);
+
+                        // Escribir los datos en la hoja de Excel
+                        worksheet.Cells.LoadFromDataTable(dataSet.Tables[0], true);
+
+                        // Aplicar formato a la tabla
+                        var tableRange = worksheet.Cells[worksheet.Dimension.Address];
+                        var table = worksheet.Tables.Add(tableRange, "Tabla1");
+
+                        // Aplicar estilo al encabezado de la tabla
+                        table.TableStyle = OfficeOpenXml.Table.TableStyles.Light1;
+
+                        // Aplicar estilo a las celdas de datos
+                        for (int row = table.Address.Start.Row + 1; row <= table.Address.End.Row; row++)
+                        {
+                            for (int col = table.Address.Start.Column; col <= table.Address.End.Column; col++)
+                            {
+                                if (row % 2 == 0)
+                                {
+                                    worksheet.Cells[row, col].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                                    worksheet.Cells[row, col].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightBlue);
+                                }
+                                else
+                                {
+                                    worksheet.Cells[row, col].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                                    worksheet.Cells[row, col].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                                }
+                            }
+                        }
+
+                        // Guardar el archivo de Excel
+                        package.SaveAs(new System.IO.FileInfo(excelFilePath));
+                    }
+
+                    Console.WriteLine("Los datos se han exportado correctamente a Excel.");
+                    connection.Close();
+                    return true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using OfficeOpenXml;
+
+namespace ExportarSQLaExcel_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Conexion conexion;
+        string connectionString;
+        string query = "";
+        string excelFilePath;
+        string NombreHoja;
+        string usuario;
+        string contraseña;
+        string servidor;
+        string db;
+        string Select;
+        string From;
+        string Where;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // Declaracion de variables
+            conexion = new Conexion();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Select != "" && From != "" && !servidor.Equals("") && !db.Equals("") && !usuario.Equals("") && !contraseña.Equals(""))
+            {
+                query = "SELECT " + Select + " FROM " + From + " WHERE " + Where;
+                connectionString = "Data Source=" + servidor + ";Initial Catalog=" + db + ";User ID=" + usuario + ";Password=" + contraseña + ";";
+
+            }
+        }
+
+        private void txtWhere_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Where = txtWhere.Text.Trim();
+        }
+
+        private void txtFrom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            From = txtFrom.Text.Trim();
+        }
+
+        private void txtSelect_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Select = txtSelect.Text.Trim();
+        }
+
+
+        private void txtUsuario_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            usuario = txtUsuario.Text.Trim();
+        }
+
+        private void txtDB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            db = txtDB.Text.Trim();
+        }
+
+        private void txtServidor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            servidor = txtServidor.Text.Trim();
+        }
+
+        private void txtContraseña_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            contraseña = txtContraseña.Password;
+        }
+
+        private void txtSelect_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtSelect.Text = txtSelect.Text.Trim();
+            Select = txtSelect.Text.Trim(); 
+        }
+
+        private void txtFrom_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtFrom.Text = txtFrom.Text.Trim();
+            From = txtFrom.Text.Trim();
+        }
+
+        private void txtWhere_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtWhere.Text = txtWhere.Text.Trim();
+            Where = txtWhere.Text.Trim();
+        }
+
+        private void txtUsuario_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtUsuario.Text = txtUsuario.Text.Trim();
+            usuario = txtUsuario.Text.Trim();
+        }
+
+        private void txtDB_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtDB.Text = txtDB.Text.Trim();
+            db = txtDB.Text.Trim();
+        }
+
+        private void txtServidor_LostFocus(object sender, RoutedEventArgs e)
+        {
+            txtServidor.Text = txtServidor.Text.Trim();
+            servidor = txtServidor.Text.Trim();
+        }
+    }
+}
